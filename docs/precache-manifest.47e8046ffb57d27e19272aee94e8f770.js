edd6f99aef7e6207cabc58290a165acdfeadac5c
miguelmerchan22 <miguelmerchan@hotmail.fr>
2021-11-02 06:56:12 -0600

    copie

---
+++ docs/precache-manifest.47e8046ffb57d27e19272aee94e8f770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9fec8211ad2d3022395ce19dea854a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c449240016a83aaf41e",
+    "url": "/static/js/2.3883aa99.chunk.js"
+  },
+  {
+    "revision": "678226323eca686164c839dfce3432f1",
+    "url": "/static/js/2.3883aa99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc7f435f963fa993b29",
+    "url": "/static/js/main.39b5a362.chunk.js"
+  },
+  {
+    "revision": "c934a238bbed439b4a4d",
+    "url": "/static/js/runtime-main.1f6c7bcd.js"
+  }
+]);
---
+++ src/cons.js
@@ -0,0 +1,13 @@
+const proxy = "https://proxy-sites.herokuapp.com/";
+
+const PRE = "https://precio-site.herokuapp.com/api/v1/servicio/precio/v2/SITE";
+
+const WS = "TN8vDJSTHXWC4hw3YJpYbnd2Rgas8a2Zre";//T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb recibe los huerfanos por defecto
+
+const SCtest = "TY5ktzXThak4nQGAfpFZxHFcAsTK1DbT1e";// direccion del contrato de pruebas test only no real
+
+const SC = "TNDDzExQSGXrHGbskYm5TJ53T3CMyRQBe1";// direccion del contrato V1
+
+const SC2 = "TNDDzExQSGXrHGbskYm5TJ53T3CMyRQBe1";// direccion del contrato V2
+
+export default {proxy, WS, SCtest, SC, SC2, PRE};
